0ebd2dbf8eb961491775ae167e9424cf490d2d90
larwong <larrywong418@gmail.com>
2022-04-04 00:52:41 -0400

    redesigned layout to include merged data

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from data import import_demo_data
 from dash import Dash, html, dcc
 from dash.dependencies import Input, Output
+from data import get_eco_indicators, get_fred_data
 
 test_options1 = ['a', 'b', 'c']
 test_options2 = ['d', 'e', 'f']
 test_value = [1, 2, 3]
+eco_indicators = get_eco_indicators()
 
 
 app = Dash(__name__)
@@ -18,56 +19,192 @@ app.layout = html.Div(
             className='row',
             children=[
                 html.Div(
-                    className='four columns div-user-controls',
-                    children=[
-                        html.H2('DASH - STOCK PRICES'),
-                        html.P('Visualising time series with Plotly - Dash.'),
-                        html.P('Pick one data type below.'),
-                    ]),
-                html.Div(
-                    className='eight columns div-for-charts bg-grey')
-            ]),
-        html.Div(
-            className='row',
-            children=[
-                html.Div(
                     className='three columns div-user-controls',
                     children=[
-                        html.H2('DATA VISUALIZATION'),
-                        html.P('Visualising ECONOMIC and TEXT data.'),
-                        html.P('Pick one data type below.'),
-                        
-                        html.Div(
-                            className='div-for-radio',
-                            children=[
-                                dcc.RadioItems(id='dataradio', options=[' Economic', ' Text'], value=' Economic'),
-                            ]),
+                        html.H2('ECONOMIC VISUALIZATION'),
+                        html.P('Visualising ECONOMIC data.'),
 
                         html.Div(
                             className='div-for-dropdown',
                             children=[
-                                dcc.Dropdown(id='datadropdown',
+                                dcc.Dropdown(id='ecodropdown1',
+                                             options=eco_indicators,
+                                             value='Real GDP',
+                                             clearable=False,
+                                             searchable=False,
+                                             style={'backgroundColor': '#1E1E1E'}),
+                            ]),
+                        html.H3('Top Left Description'),
+                        
+                        html.Div(id='ecodesc1'),
+                        
+                        html.Br(),
+                        
+                        html.Div([
+                                html.Button('SWAP', id='btn-swap', n_clicks=0, style={'width': '100%', 'color': 'white'}),
+                            ]),
+                        
+                        html.Br(),
+                        
+                        html.Div(
+                            className='div-for-dropdown',
+                            children=[
+                                dcc.Dropdown(id='ecodropdown2',
+                                             options=eco_indicators,
+                                             value='Real GDP',
+                                             clearable=False,
+                                             searchable=False,
+                                             style={'backgroundColor': '#1E1E1E'}),
+                            ]),
+                        
+                        
+                        html.H3('Top Right Description'),
+                        
+                        html.Div(id='ecodesc2'),
+                        # html.Div(),
+                    ]),
+                html.Div(
+                    className='nine columns bg-grey',
+                    children=[
+                        html.Div([
+                                html.Div([dcc.Graph(id='ecofig1', 
+                                        figure={
+                                            'layout': {
+                                                'plot_bgcolor': '#1E1E1E',
+                                                'paper_bgcolor': '#1E1E1E',
+                                                'font': {
+                                                    'color': 'white'
+                                                }
+                                            }
+                                        }
+                                )], style={'width': '50%'}),
+                                html.Div([dcc.Graph(id='ecofig2', 
+                                        figure={
+                                            'layout': {
+                                                'plot_bgcolor': '#1E1E1E',
+                                                'paper_bgcolor': '#1E1E1E',
+                                                'font': {
+                                                    'color': 'white'
+                                                }
+                                            }
+                                        }
+                                )], style={'width': '50%'}),
+                        ], style={'display': 'flex', 'width': '100%'}),
+                        dcc.Graph(id='ecomerge', 
+                                      figure={
+                                        'layout': {
+                                            'plot_bgcolor': '#1E1E1E',
+                                            'paper_bgcolor': '#1E1E1E',
+                                            'font': {
+                                                'color': 'white'
+                                            }
+                                        }
+                                      }
+                            ),
+                    ], style={'padding': '10px 5px', 'white-space': 'nowrap'}
+                    ), 
+            ]),
+        
+        
+        html.Div(
+            className='row',
+            children=[
+                html.Div(
+                    className='four columns div-user-controls',
+                    children=[
+                        html.H2('TEXT VISUALIZATION'),
+                        html.P('Visualising TEXT data.'),
+
+                        html.Div(
+                            className='div-for-dropdown',
+                            children=[
+                                dcc.Dropdown(id='textdropdown',
+                                             options=eco_indicators,
                                              searchable=False,
                                              style={'backgroundColor': '#1E1E1E'}),
                             ]),
                     ]),
             html.Div(
-                className='nine columns div-for-charts bg-grey',
+                className='eight columns div-for-charts bg-grey',
                 children=[])
-            ])
-    ])
+            ]),
+])
 
+@app.callback(
+    Output('ecodropdown1', 'value'),
+    Output('ecodropdown2', 'value'),
+    Input('btn-swap', 'n_clicks')
+)
+def swap_graphs(ind1, ind2):
+    print('yo')
+    return ind2, ind1
 
-@app.callback(Output('datadropdown', 'options'), Input('dataradio', 'value'))
-
-def dropdown_options(radio_value):
+@app.callback(
+    Output('ecodesc1', 'children'),
+    Output('ecofig1', 'figure'),
+    Input('ecodropdown1', 'value')
+)
+def output_fig(ind):
+    fred_info = get_fred_data(ind)
+    metadata = fred_info['metadata']
+    data = fred_info['data']
     
-    if radio_value == ' Economic':
-        options = [{'label': x, 'value': x} for x in test_options1]
-    else:
-        options = [{'label': x, 'value': x} for x in test_options2]
+    data_description = ['{}'.format(metadata['title'][0]),
+                        'Unit: {}'.format(metadata['units'][0]),
+                        'Frequency: {}'.format(metadata['frequency'][0]),
+                        'Last Updated: {}'.format(metadata['last_updated'][0])]
+
+    fig = px.line(data, x='Date', y=metadata['units'][0], title=ind)
+    fig.update_traces(line_color='#ff0000', hovertemplate=None)
     
-    return options
+    fig.update_layout(
+        transition_duration=500,
+        title_font_family="Times New Roman",
+        title_font_color='white',
+        title_font_size=25,
+        font_color='white',
+        plot_bgcolor='#1E1E1E',
+        paper_bgcolor='#1E1E1E',
+    )
+    
+    # Add range slider
+    fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                        label="1m",
+                        step="month",
+                        stepmode="backward"),
+                    dict(count=6,
+                        label="6m",
+                        step="month",
+                        stepmode="backward"),
+                    dict(count=1,
+                        label="YTD",
+                        step="year",
+                        stepmode="todate"),
+                    dict(count=1,
+                        label="1y",
+                        step="year",
+                        stepmode="backward"),
+                    dict(step="all")
+                ]),
+                font=dict(color='black')
+            ),
+            rangeslider=dict(
+                visible=True
+            ),
+            type="date"
+        )
+)
+
+    date_min = pd.to_datetime(data['Date']).min()
+    date_max = pd.to_datetime(data['Date']).max()
+    slider_value = data['Date'].max()
+    
+    
+    return [html.P(x) for x in data_description], fig
 
 
 if __name__ == '__main__':
--- data.py
+++ data.py
@@ -6,16 +6,43 @@ from fredapi import Fred
 ECONOMIC_INDICATORS = json.load(open('assets/identifiers.json'))['economic_indicators']
 FRED = Fred(FRED_API_KEY)
 
+def get_eco_indicators():
+    """
+    Get list of economic indicators from json
+    """
+    return list(ECONOMIC_INDICATORS.keys())
+
 def get_fred_data(indicator, drop=True):
+    """
+    Quwery Fred API for series data
+    """
     if indicator not in ECONOMIC_INDICATORS:
         return {'status': False, 'metadata': None, 'data': None}
 
     metadata = FRED.search(ECONOMIC_INDICATORS[indicator])
-    data = FRED.get_series_latest_release(ECONOMIC_INDICATORS[indicator])
-    data.columns = ['Data', metadata['units']]
+    data = FRED.get_series_latest_release(ECONOMIC_INDICATORS[indicator]).to_frame().reset_index(drop=False)
+    data.columns = ['Date', metadata['units'][0]]
     return {'status': True , 'metadata': metadata, 'data': data.dropna() if drop else data}
 
+def get_paired_data(first_ind, second_ind):
+    """
+    Merge two economic indicator data based on common dates of measurement
+    """
+    if first_ind not in ECONOMIC_INDICATORS or second_ind not in ECONOMIC_INDICATORS:
+        return None
+    
+    first_ind_data = get_fred_data(first_ind)
+    second_ind_data = get_fred_data(second_ind)
+    
+    merged = pd.merge(first_ind_data['data'], second_ind_data['data'], how='inner', on=['Date','Date'])
+    
+    return {'status': True, 'indicators': [first_ind_data, second_ind_data], 'merged': merged}
+
 def search_fred(indicator):
+    """
+    Search Fred API for relevant economic indicators
+    For testing purposes
+    """
     try:
         metadata = FRED.search(ECONOMIC_INDICATORS[indicator])
         return {'status': True , 'metadata': metadata}
